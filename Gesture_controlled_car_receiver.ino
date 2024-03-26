8b3b08c362176e08b14d32304684b320c9888039
K4nishkk <115213375+K4nishkk@users.noreply.github.com>
2024-03-26 19:53:22 +0530

    Update Gesture_controlled_car_receiver.ino

--- Gesture_controlled_car_receiver.ino
+++ Gesture_controlled_car_receiver.ino
@@ -1,221 +1,110 @@
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
 #include <esp_now.h>
 #include <WiFi.h>
-#include <ezButton.h>
 
-// REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = { 0x94, 0xB5, 0x55, 0xF4, 0x31, 0x38 };
+// control pins
+#define IN_1  12
+#define IN_2  14
+#define IN_3  27
+#define IN_4  26
 
+// struct to receive data
 typedef struct struct_message {
-  uint8_t num;
+    uint8_t num;
 } struct_message;
-
-// Create a struct_message called myData
 struct_message myData;
 
-esp_now_peer_info_t peerInfo;
+// too keep track of connection
+int prevTime = 0;
+int currTime = 0;
 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
+void goAhead(){ 
+      Serial.println("Forward");
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
 
-MPU6050 mpu;
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+  }
 
-#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+void goBack(){ 
+      Serial.println("Reverse");
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
 
-// MPU control/status vars
-bool dmpReady = false;   // set true if DMP init was successful
-uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
-uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;      // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64];  // FIFO storage buffer
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+  }
 
-// orientation/motion vars
-Quaternion q;         // [w, x, y, z]         quaternion container
-VectorFloat gravity;  // [x, y, z]            gravity vector
-float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+void goRight(){ 
+      Serial.println("Right");
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
 
-# define CONNECTION_LED 27
-bool connection;
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+  }
 
-ezButton button(33);
+void goLeft(){
+      Serial.println("Left");
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
 
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+  }
 
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
+void stopRobot(){  
+      Serial.println("Stopped");
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, LOW);
 
-volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-  mpuInterrupt = true;
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, LOW);
+ }
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  currTime = millis();
+  memcpy(&myData, incomingData, sizeof(myData));
+
+  if (myData.num == 0) {
+    stopRobot();
+  }
+  else if (myData.num == 1) {
+    goAhead();
+  }
+  else if (myData.num == 2) {
+    goBack();
+  }
+  else if (myData.num == 3) {
+    goLeft();
+  }
+  else if (myData.num == 4) {
+    goRight();
+  }
 }
-
-
-
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
-
+ 
 void setup() {
-// join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  Wire.begin();
-  Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
-#endif
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT); 
 
-  // initialize serial communication
-  // (115200 chosen because it is required for Teapot Demo output, but it's
-  // really up to you depending on your project)
   Serial.begin(115200);
-  while (!Serial)
-    ;  // wait for Leonardo enumeration, others continue immediately
-
-  // Set device as a Wi-Fi Station
+  
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  // esp_now_register_send_cb(OnDataSent);
-
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-
-  // Add peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-    return;
-  }
-
-  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
-  // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
-  // the baud timing being too misaligned with processor ticks. You must use
-  // 38400 or slower in these cases, or use some kind of external separate
-  // crystal solution for the UART timer.
-
-  // initialize device
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
-
-  // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  delay(500);
-
-  // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-  // accel and gyro offsets
-  mpu.setXAccelOffset(-3548);
-  mpu.setYAccelOffset(-1381);
-  mpu.setZAccelOffset(723);
-  mpu.setXGyroOffset(27);
-  mpu.setYGyroOffset(30);
-  mpu.setZGyroOffset(-11);
-
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    mpu.PrintActiveOffsets();
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-
-    // enable Arduino interrupt detection
-    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-    Serial.println(F(")..."));
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    dmpReady = true;
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
-
-  connection = true;
-  button.setDebounceTime(50);  // set debounce time to 50 milliseconds
-  pinMode(27, OUTPUT);
-  digitalWrite(27, HIGH);
+  esp_now_register_recv_cb(OnDataRecv);
 }
-
-
-
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
-
+ 
 void loop() {
-  // if programming failed, don't try to do anything
-  if (!dmpReady) return;
-  // read a packet from FIFO
-  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {  // Get the Latest packet
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-    // Serial.print("pitch: ");
-    // Serial.print(ypr[1] * 180/M_PI);
-    // Serial.print("\troll: ");
-    // Serial.println(ypr[2] * 180/M_PI);
-
-    if (ypr[1] > 0.4) {
-      Serial.println("Right");
-      myData.num = 4;
-    } else if (ypr[1] < -0.4) {
-      Serial.println("Left");
-      myData.num = 3;
-    } else if (ypr[2] > 0.4) {
-      Serial.println("Forward");
-      myData.num = 1;
-    } else if (ypr[2] < -0.4) {
-      Serial.println("Reverse");
-      myData.num = 2;
-    } else {
-      Serial.println("Stop");
-      myData.num = 0;
-    }
-
-    button.loop();  // MUST call the loop() function first
-
-    if (button.isReleased()) {
-      connection = !connection;
-      if (connection == true) digitalWrite(27, HIGH);
-      else digitalWrite(27, LOW);
-    }
-
-    if (connection == false) {
-      myData.num = 0;
-    }
-
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+  if (prevTime == currTime) {
+    stopRobot();
   }
+  prevTime = currTime;
+  delay(1000);
 }
