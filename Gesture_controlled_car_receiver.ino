52dbcd173d18cddabad6c5f1565bec69b3a8cf6f
K4nishkk <115213375+K4nishkk@users.noreply.github.com>
2024-03-29 19:14:11 +0530

    Update Gesture_controlled_car_receiver.ino

--- Gesture_controlled_car_receiver.ino
+++ Gesture_controlled_car_receiver.ino
@@ -1,15 +1,67 @@
+// ******************************************** HEADER FILES *************************************
 #include <esp_now.h>
 #include <WiFi.h>
 
-// control pins
-#define IN_1  12
-#define IN_2  14
-#define IN_3  27
-#define IN_4  26
+// ************************************** VARIABLES AND FUNCTIONS ********************************************
 
-// struct to receive data
+// ======================== MOTOR CONTROL ============================
+// control pins
+#define ENA 33
+#define ENB 32
+#define IN_1 12
+#define IN_2 14
+#define IN_3 27
+#define IN_4 26
+
+// Setting PWM properties
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+
+void goAhead() {
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+}
+
+void goBack() {
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+}
+
+void goRight() {
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+}
+
+void goLeft() {
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+}
+
+void stopRobot() {
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, LOW);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, LOW);
+}
+
+// ============================= ESP-NOW ===================================
 typedef struct struct_message {
-    uint8_t num;
+  uint8_t dir;
+  uint8_t speed;
 } struct_message;
 struct_message myData;
 
@@ -17,80 +69,49 @@ struct_message myData;
 int prevTime = 0;
 int currTime = 0;
 
-void goAhead(){ 
-      Serial.println("Forward");
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-  }
-
-void goBack(){ 
-      Serial.println("Reverse");
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-  }
-
-void goRight(){ 
-      Serial.println("Right");
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-  }
-
-void goLeft(){
-      Serial.println("Left");
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-  }
-
-void stopRobot(){  
-      Serial.println("Stopped");
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, LOW);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, LOW);
- }
-
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   currTime = millis();
   memcpy(&myData, incomingData, sizeof(myData));
 
-  if (myData.num == 0) {
+  int dutyCycle = map(myData.speed, 1, 6, 180, 255);
+  ledcWrite(pwmChannel, dutyCycle);
+
+  if (myData.dir == 0) {
     stopRobot();
-  }
-  else if (myData.num == 1) {
+  } else if (myData.dir == 1) {
     goAhead();
-  }
-  else if (myData.num == 2) {
+  } else if (myData.dir == 2) {
     goBack();
-  }
-  else if (myData.num == 3) {
+  } else if (myData.dir == 3) {
     goLeft();
-  }
-  else if (myData.num == 4) {
+  } else if (myData.dir == 4) {
     goRight();
   }
+
+  Serial.print(myData.dir);
+  Serial.print("\t");
+  Serial.println(myData.speed);
 }
- 
+
+// ************************************************ SETUP ***********************************************
+
 void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
   pinMode(IN_1, OUTPUT);
   pinMode(IN_2, OUTPUT);
   pinMode(IN_3, OUTPUT);
-  pinMode(IN_4, OUTPUT); 
+  pinMode(IN_4, OUTPUT);
+  
+  // configure LED PWM functionalitites
+  ledcSetup(pwmChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ENA, pwmChannel);
+  ledcAttachPin(ENB, pwmChannel);
 
   Serial.begin(115200);
-  
+
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != ESP_OK) {
@@ -100,9 +121,12 @@ void setup() {
 
   esp_now_register_recv_cb(OnDataRecv);
 }
- 
+
+// ************************************************ LOOP *************************************************
+
 void loop() {
   if (prevTime == currTime) {
+    Serial.println("Not Connected");
     stopRobot();
   }
   prevTime = currTime;
